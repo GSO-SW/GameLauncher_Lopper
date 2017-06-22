aafe25e27da5f4a746404a9048728bbbd976b01c
lopper92 <iah41lopper@gso-koeln.de>
2017-06-22 13:55:13 +0200

    Testcommit

--- GameLauncher/GameLauncher.cs
+++ GameLauncher/GameLauncher.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GameLauncher
 {
-    public partial class Form1 : Form
+    public partial class GameLauncher : Form
     {
-        public Form1()
+        public GameLauncher()
         {
             InitializeComponent();
         }

5bdba0a68ea2df98ab898ab494bf8b419ccd2053
lopper92 <iah41lopper@gso-koeln.de>
2017-06-22 15:14:07 +0200

    Erste Tests erfolgreich

---
+++ GameLauncher/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLauncher
+{
+    public class Game
+    {
+        public string title;
+        public string path;
+
+        public Game(string titel, string pfad)
+        {
+            title = titel;
+            path = pfad;
+            try
+            {
+                if (!File.Exists(path))
+                    throw new FileNotFoundException();
+            }
+            catch(FileNotFoundException e)
+            {
+                throw;
+            }
+            
+            //throw new NotImplementedException();
+        }
+        
+    }
+}
